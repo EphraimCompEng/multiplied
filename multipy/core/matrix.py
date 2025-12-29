87afaaf21afdcc3046258199478364702174c809
EphraimCompEng <madugba.finance@proton.me>
2025-12-29 17:18:36 +0000

    Refactor begins. Un-bumping version until: truth, template and matrix modules are complete

--- multipy/core/matrix.py
+++ multipy/core/matrix.py
@@ -1,3 +1,55 @@
 #################################################
 # Generating, Aligning Initial Partial Products #
 #################################################
+
+
+
+class Matrix:
+    def __init__(self, bits: int):
+        valid_range = {4, 8}
+        if bits in valid_range:
+            self.bits = bits
+        else:
+            raise ValueError(f"Valid bit lengths: {valid_range}")
+        self.matrix = self.build_matrix()
+
+    def build_matrix(self) -> list[list[int]]:
+        """
+        Build a logic AND matrix for a bitwidth of self.bits. For example:
+        >>> self.bits = 4
+        >>> build_matrix()
+        [['_','_','_','_',0,0,0,0],
+         ['_','_','_',0,0,0,0,'_'],
+         ['_','_',0,0,0,0,'_','_'],
+         ['_',0,0,0,0,'_','_','_']]
+        """
+        row = [0]*self.bits
+        matrix = []
+        for i in range(self.bits):
+            matrix.append(["_"]*(self.bits-i) + row + ["_"]*i)
+        return matrix
+
+
+    def __repr__(self) -> str:
+        """
+        Format self.matrix as a string:
+
+        "____0000\n
+         ___0000_\n
+         __0000__\n
+         _0000___"
+        """
+        pretty = ""
+        for i in self.matrix:
+            row = [str(x) for x in i]
+            pretty += "".join(row) + "\n"
+        return pretty
+
+def main():
+    matrix4 = Matrix(4)
+    print(matrix4)
+    matrix8 = Matrix(8)
+    print(matrix8)
+
+if __name__ == "__main__":
+    main()
--- multipy/core/template.py
+++ multipy/core/template.py
@@ -1,3 +1,21 @@
 ################################################
 # Returns Template Objects Using User Patterns #
 ################################################
+
+
+
+"""
+Simple templates should be represented as a list with each element on
+a new line, this makes it clear how each layer is reduced:
+>>> my_simple_template = [
+    1,
+    1,
+    2,
+    2,
+    2,
+    ...
+]
+
+The "run" of a given element determines where adders, run=2, or a
+combination of CSAs and HAs, run=3, are used.
+"""

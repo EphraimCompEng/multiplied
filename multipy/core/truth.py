809ce46e3f95bdd56a804eece929b19973faf01e
EphraimCompEng <madugba.finance@proton.me>
2025-12-29 18:01:09 +0000

    Stategies outlined in doc strings

--- multipy/core/truth.py
+++ multipy/core/truth.py
@@ -5,3 +5,11 @@
 from pprint import pprint
 import template
 import itertools
+
+
+"""
+Do not optimise generation until functionality is actually tested for
+edge cases and speed. Then refactor by using appropriate patterns,
+simplification, etc., before applying multiprocessing and beyond.
+
+"""

22e54d31601bbf9bb5d3a359a94c4a0896ab2976
EphraimCompEng <madugba.finance@proton.me>
2026-01-17 02:07:46 +0000

    Implement char generator

--- multiplied/core/utils/char.py
+++ multiplied/core/utils/char.py
@@ -1,11 +1,8 @@
-
-
-
-
-
+from collections.abc import Generator
 
 
 # --  Character related helper functions ----------------------------
+
 def ischar(ch:str) -> bool:
     """
     Tests if a string is exactly one alphabetic character
@@ -15,3 +12,21 @@ def ischar(ch:str) -> bool:
         return True
     except (ValueError, TypeError):
         return False
+
+def chargen() -> Generator[str]:
+    i = 0
+    while True:
+        yield chr((i % 26) + 65)
+        i += 1
+
+
+
+
+# -- testing --------------------------------------------------------
+def main():
+    test = chargen()
+    for _ in range(32):
+        print(next(test))
+
+if __name__ == "__main__":
+    main()
